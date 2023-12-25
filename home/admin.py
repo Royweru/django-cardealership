2004da4084450a32ba2f7cf4770adeaf715cf3d5
Royweru <royezi17@gmail.com>
2023-12-25 16:36:49 +0300

    modified:   home/__pycache__/admin.cpython-311.pyc 	modified:   home/admin.py

--- home/admin.py
+++ home/admin.py
@@ -7,8 +7,17 @@ from .models import Model,Make,BodyType,Car,Image
 class CarAdmin(admin.ModelAdmin):
     list_display=('model','make','body','isAvailable','isFeatured','locationValue','mileage','engineSize','drive','transmission','horsePower','acceleration','fuelType')
 
+class MakeAdmin(admin.ModelAdmin):
+    list_display=('name','image')
+    
+# class ModelAdmin(admin.ModelAdmin):
+#     list_display=('name')
+    
+# class BodyTypeAdmin(admin.ModelAdmin):
+#     list_display=('name')
+
 admin.site.register(Model)
-admin.site.register(Make)
+admin.site.register(Make,MakeAdmin)
 admin.site.register(BodyType)
 admin.site.register(Car)
 admin.site.register(Image)
