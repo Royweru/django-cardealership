da14c0e457c07e4eeb955831ec59cbf571448f6f
Royweru <royezi17@gmail.com>
2023-12-25 12:06:58 +0300

    modified:   carshub/settings.py

--- carshub/settings.py
+++ carshub/settings.py
@@ -83,6 +83,9 @@ DATABASES = {
     }
 }
 
+MEDIA_ROOT = ''
+MEDIA_URL = ''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -119,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[
+    BASE_DIR/'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
